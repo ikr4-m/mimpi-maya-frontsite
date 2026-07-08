774a3332ae43b2c7f1f717348876b63d542030c5
Ikramullah Latif <contact@ikr4m.my.id>
2026-07-08 21:01:11 +0700

    Add phpblock for Models

--- app/Models/AuditionContent.php
+++ app/Models/AuditionContent.php
@@ -5,6 +5,38 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $type
+ * @property string|null $title
+ * @property string|null $description
+ * @property string|null $label
+ * @property string|null $url
+ * @property string|null $icon
+ * @property \Illuminate\Support\Carbon|null $date
+ * @property int $sort_order
+ * @property bool $is_active
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static Builder<static>|AuditionContent active()
+ * @method static Builder<static>|AuditionContent byType(string $type)
+ * @method static Builder<static>|AuditionContent newModelQuery()
+ * @method static Builder<static>|AuditionContent newQuery()
+ * @method static Builder<static>|AuditionContent query()
+ * @method static Builder<static>|AuditionContent whereCreatedAt($value)
+ * @method static Builder<static>|AuditionContent whereDate($value)
+ * @method static Builder<static>|AuditionContent whereDescription($value)
+ * @method static Builder<static>|AuditionContent whereIcon($value)
+ * @method static Builder<static>|AuditionContent whereId($value)
+ * @method static Builder<static>|AuditionContent whereIsActive($value)
+ * @method static Builder<static>|AuditionContent whereLabel($value)
+ * @method static Builder<static>|AuditionContent whereSortOrder($value)
+ * @method static Builder<static>|AuditionContent whereTitle($value)
+ * @method static Builder<static>|AuditionContent whereType($value)
+ * @method static Builder<static>|AuditionContent whereUpdatedAt($value)
+ * @method static Builder<static>|AuditionContent whereUrl($value)
+ * @mixin \Eloquent
+ */
 class AuditionContent extends Model
 {
     protected $fillable = [
--- app/Models/AuditionSetting.php
+++ app/Models/AuditionSetting.php
@@ -4,6 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string|null $form_url
+ * @property \Illuminate\Support\Carbon $audition_start
+ * @property \Illuminate\Support\Carbon $audition_end
+ * @property string|null $tagline
+ * @property string|null $about_title
+ * @property string|null $about_description
+ * @property bool $is_active
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|AuditionSetting newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|AuditionSetting newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|AuditionSetting query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|AuditionSetting whereAboutDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|AuditionSetting whereAboutTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|AuditionSetting whereAuditionEnd($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|AuditionSetting whereAuditionStart($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|AuditionSetting whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|AuditionSetting whereFormUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|AuditionSetting whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|AuditionSetting whereIsActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|AuditionSetting whereTagline($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|AuditionSetting whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class AuditionSetting extends Model
 {
     protected $fillable = [
